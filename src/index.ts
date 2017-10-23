a46e756e61920beb09fc7c0a6d7ad248eaabeaab
veinsit <vanes.parma@gmail.com>
2017-10-23 20:31:16 +0200

    tsc

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+// per typescript usare : npm install @types/node --save-dev
+
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const TOKEN = process.env.TELEGRAM_TOKEN
+
+var useWebhook = true; // se false usa polling
+
+var options
+if (useWebhook) {
+  options = {
+    webHook: {
+      // Port to which you should bind is assigned to $PORT variable
+      // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
+      port: process.env.PORT
+      // you do NOT need to set up certificates since Heroku provides
+      // the SSL certs already (https://<app-name>.herokuapp.com)
+      // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
+    }
+  };
+}
+else {
+  options = {
+    polling: true //, WebHook and Polling are mutually exclusive
+  };
+}
+  // Heroku routes from port :443 to $PORT
+  // Add URL of your app to env variable or enable Dyno Metadata
+  // to get this automatically
+  // See: https://devcenter.heroku.com/articles/dyno-metadata
+  const url = process.env.APP_URL || 'https://vanesplayingbot.herokuapp.com';
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(TOKEN, options);
+
+// This informs the Telegram servers of the new webhook.
+// Note: we do not need to pass in the cert, as it already provided
+if (useWebhook)
+   bot.setWebHook(`${url}/bot${TOKEN}`);
+
+
+//=========================== messaggi
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
